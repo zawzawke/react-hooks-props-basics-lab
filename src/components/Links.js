5e8fb02f0749688f852fe00ee5a0bba2cede1023
Paul <paulallanmain@gmail.com>
2025-04-12 02:40:28 +0300

    Complete lab

--- src/components/Links.js
+++ src/components/Links.js
@@ -5,7 +5,6 @@ function Links({github, linkedin}) {
         <div>
             <h3>Links</h3>
             <a href={github}>{github}</a>
-            <br />
             <a href={linkedin}>{linkedin}</a>
         </div>
     )
